dbf8eab7bb4f59c7d3e08c3566552ea801dd164c
victoriamaximenko13 <victoria_maximenko@icloud.com>
2022-08-17 18:27:47 +0300

    Добавила решение задания в script.js

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-/* Новые элементы должны добавляться в список по нажатию на Enter */
+const input = document.querySelector("#input");
+const items = document.querySelector(".items");
 
-/* Пустые элементы не должны добавляться */
+input.addEventListener("keydown", function (event) {
+    if (event.key == "Enter") {
+        if (input.value != "") {
+            const item = document.createElement("div");
+            const itemTitle = input.value;
+            item.classList.add("item");
+            item.textContent = itemTitle;
+            items.append(item);
+            input.value = "";
+            item.addEventListener("click", function () {
+                item.classList.toggle("done");
+            })
+        }
+    }
+})
 
-/* Если кликнуть на элемент списка, он зачеркивается */
-
-/* Если кликнуть повторно уже на зачеркнутый, он снова становится обычным */
-
-/* Очищать input после добавления нового элемента в список */
